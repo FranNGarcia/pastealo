0cd9048aa84b2cbaaebe225490106dd590ced9ba
Francisco <frgarcia@dc.uba.ar>
2025-02-20 19:45:07 -0300

    Servidor con autoping para evitar idle

--- pastealo-frontend/src/services/pasteAPI.js
+++ pastealo-frontend/src/services/pasteAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://pastealo.onrender.com/paste';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getAllPastes = async () => {
     try {
