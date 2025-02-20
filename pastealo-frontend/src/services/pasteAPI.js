806ffc10c4a01a92bd27f2e347391239746d38c4
Francisco <frgarcia@dc.uba.ar>
2025-02-20 20:37:43 -0300

    cambio en url

--- pastealo-frontend/src/services/pasteAPI.js
+++ pastealo-frontend/src/services/pasteAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL + '/pastes';
+const API_URL = process.env.REACT_APP_API_URL + '/paste';
 
 export const getAllPastes = async () => {
     try {
